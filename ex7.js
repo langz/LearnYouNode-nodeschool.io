0da25d36d049e0b2a69e4880f6effd2551c25bb1
AndersLangseth <anderslangseth@hotmail.com>
2014-06-03 21:36:52 +0200

    Fullført Oppgave7

---
+++ ex7.js
@@ -0,0 +1,18 @@
+var url = process.argv[2];
+var http = require('http');
+http.get(url, function(response){
+
+	response.setEncoding("utf8");
+
+	response.on("data", function(data){
+		console.log(data);
+	});
+
+	response.on("error", function(error){
+
+	});
+
+	response.on("end", function(data){
+
+	});
+});
